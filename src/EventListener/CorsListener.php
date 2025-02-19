13cbe1257912f92af54b0a01bdb1ff2d6dce6110
AntonDelahaye <anton.delahaye@itautomotive.fr>
2025-02-19 14:46:24 +0100

    CORS correction 2

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -15,7 +15,7 @@ class CorsListener
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
 
         if ($request->isMethod('OPTIONS')) {
             $event->setResponse(new Response('', 200, $response->headers->all()));
