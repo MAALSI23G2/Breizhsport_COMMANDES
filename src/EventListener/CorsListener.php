470288cbdfd0262353efafb7f45fbd8cc53a72ce
AntonDelahaye <anton.delahaye@itautomotive.fr>
2025-02-19 14:39:08 +0100

    CORS correction

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -2,16 +2,24 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
 
 class CorsListener
 {
     public function onKernelResponse(ResponseEvent $event): void
     {
         $response = $event->getResponse();
+        $request = $event->getRequest();
+
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        if ($request->isMethod('OPTIONS')) {
+            $event->setResponse(new Response('', 200, $response->headers->all()));
+        }
     }
 }
 
